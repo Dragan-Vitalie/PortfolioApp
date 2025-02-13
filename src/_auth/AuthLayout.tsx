4d8d42d5763c4451e1a47b4f225e8d6763703ac6
Dragan-Vitalie <vitalie.dragan@songkick.com>
2025-02-13 06:46:52 +0000

    add necesary icons & work on AuthLayout page

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -9,9 +9,18 @@ function AuthLayout() {
         <Navigate to='/' />
       ):(
         <>
-          <section>
+          <section className='flex flex-1 justify-center items-center flex-col py-10'>
             <Outlet />
           </section>
+
+          <img 
+            src='/assets/images/side-img.svg'
+            alt='logo'
+            className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat'
+            // src='/assets/images/portfolio.png'
+            // alt='logo'
+            // className='hidden xl:block h-1/3 w-1/2 object-cover bg-no-repeat'
+          />
         </>
       )}
     </>

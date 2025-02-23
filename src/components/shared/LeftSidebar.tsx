ec8faf3a9b426cda7b2550abe3788c50049e54b2
Dragan-Vitalie <vitalie.dragan@songkick.com>
2025-02-23 01:16:35 +0000

    add logout on the left sidebar

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -67,6 +67,13 @@ function LeftSidebar() {
           })}
         </ul>
       </div>
+      <Button variant='ghost' className='shad-button_ghost' onClick={() => signOut()}>
+        <img 
+          src='/assets/icons/logout.svg'
+          alt='logout'
+        />
+        <p className='small-medium lg:base-medium'>Logout</p>
+      </Button>
     </section>
   )
 }
